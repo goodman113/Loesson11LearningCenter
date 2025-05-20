dc14b48bd668c4c807c5d6462feaae78b9baeea0
Nurilloh <formylaptop2009@example.com>
2025-05-20 19:21:30 +0500

    as

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -2,9 +2,37 @@ package service;
 
 import entity.User;
 
+import static db.DataSource.*;
 import static db.DataSource.students;
 
 public class StudentService {
+public static void service(){
+
+    while (true) {
+        System.out.println("""
+                    0 exit
+                    1 My marks
+                    2 My attendance
+                    """);
+
+
+        switch (strScanner.nextLine()) {
+            case "0" -> {
+                return;
+            }
+            case "1" -> myMarks();
+            case "2" -> myAttendance();
+        }
+    }
+
+}
+
+    private static void myAttendance() {
+
+    }
+
+    private static void myMarks() {
+    }
 
     public static void showStudents(){
         for (User student : students) {
