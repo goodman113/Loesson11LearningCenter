c27007c580b2d96fe970110bf578ffea4223e4a7
Nurilloh <formylaptop2009@example.com>
2025-05-20 16:00:22 +0500

    DataSource

---
+++ src/db/DataSource.java
@@ -0,0 +1,19 @@
+package db;
+
+import entity.User;
+import entity.enums.Role;
+
+import java.util.Scanner;
+
+public class DataSource {
+    public static Scanner scanner  =new Scanner(System.in);
+    public static Scanner strScanner  =new Scanner(System.in);
+    public static User  currentUser = null;
+    static {
+        User admin = new User("admin","admin","admin", Role.ADMIN);
+        User teacher1 = new User("teacher1","teacher1","teacher1", Role.STUDENT);
+        User teacher2 = new User("teacher2","teacher2","teacher2", Role.STUDENT);
+        User a = new User("a","a","a", Role.STUDENT);
+        User b = new User("b","b","b", Role.STUDENT);
+    }
+}
