5131b6581b7c58cda83da42dd9864a946aa4e1a3
Nurilloh <formylaptop2009@example.com>
2025-05-20 17:38:58 +0500

    entity filled

--- src/entity/StudentAttendanceDaily.java
+++ src/entity/StudentAttendanceDaily.java
@@ -6,4 +6,34 @@ public class StudentAttendanceDaily {
     private String studentId;
     private String studentName;
     private AttendanceStatus status;
+
+    public StudentAttendanceDaily(String studentId, String studentName, AttendanceStatus status) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.status = status;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public AttendanceStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AttendanceStatus status) {
+        this.status = status;
+    }
 }
