d1fe7b28eedf03c8c0770333735d87a7a32c419d
Rovwan <abduvaliyevrovshan6@gmail.com>
2025-05-20 16:05:33 +0500

    admin

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -1,4 +1,28 @@
 package service;
 
+import static db.DataSource.scanner;
+
 public class AdminService {
+
+    public void menu()
+    {
+        while (true)
+        {
+            System.out.println("""
+                    0 exit
+                    1 add teacher
+                    2 add student
+                    3 edit techer/student
+                    4 delete techer/student
+                    5 create group
+                    6 edit group
+                    """);
+            switch (scanner.nextInt())
+            {
+
+            }
+
+        }
+    }
+
 }
